8d60d66b72ce827574dd17cf3b767453d366b6bc
chou0728 <dean34520@gmail.com>
2019-06-01 18:33:13 +0800

    Add posts pages

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,73 @@
+<style scoped lang="scss">
+.single-post-page {
+	padding: 30px;
+	text-align: center;
+	box-sizing: border-box;
+}
+
+.post {
+	width: 100%;
+}
+
+@media (min-width: 768px) {
+	.post {
+		width: 600px;
+		margin: auto;
+	}
+}
+
+.post-title {
+	margin: 0;
+}
+
+.post-details {
+	padding: 10px;
+	box-sizing: border-box;
+	border-bottom: 3px solid #ccc;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+}
+
+@media (min-width: 768px) {
+	.post-details {
+		flex-direction: row;
+	}
+}
+
+.post-detail {
+	color: rgb(88, 88, 88);
+	margin: 0 10px;
+}
+
+.post-feedback a {
+	color: red;
+	text-decoration: none;
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+	color: salmon;
+}
+</style>
+
+<template>
+	<div class="single-post-page">
+		<section class="post">
+			<h1 class="post-title">title</h1>
+			<div class="post-details">
+				<div class="post-detail">最近更新日期: XXX</div>
+				<div class="post-detail">作者: XXX</div>
+			</div>
+			<p class="post-content">內容</p>
+			<button type="button" class="btn btn-primary">Primary</button>
+		</section>
+		<section class="post-feedback">
+			<p>
+				有疑問可email至
+				<a href="mailto:dean34520@gmail.com">dean34520@gmail.com</a>
+			</p>
+		</section>
+	</div>
+</template>
---
+++ pages/posts/index.vue
@@ -0,0 +1,11 @@
+<style scoped lang="scss">
+.posts-page {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
+
+<template>
+	<div class="posts-page"></div>
+</template>
